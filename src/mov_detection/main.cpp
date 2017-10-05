a566d4fc97f1ce743f54b2925403e70120f5949b
Qjammer <baladoramiro+github@gmail.com>
2017-10-05 21:31:24 +0200

    reduced influence of higher frequencies, makes it more robust

--- src/mov_detection/main.cpp
+++ src/mov_detection/main.cpp
@@ -6,24 +6,29 @@
 
 
 int main(int argc, char* argv[]){
-	const char* filename1 = "test_images/test2_1.jpg";
-	const char* filename2 = "test_images/test2_2.jpg";
+	const char* filename1 = "test_images/test1_1.jpg";
+	const char* filename2 = "test_images/test1_2.jpg";
 
 	cv::Mat I1 = cv::imread(filename1, cv::IMREAD_GRAYSCALE);
-	int size=2500;
-	cv::Rect r={I1.cols/2-size/2,I1.rows/2-size/2,size,size};
-	cv::Mat mg1= I1(r);
+	int sizeA=2500;
+	cv::Rect rA={I1.cols/2-sizeA/2,I1.rows/2-sizeA/2,sizeA,sizeA};
+	cv::Mat mg1= I1(rA);
 	cv::Mat f1=fourierTrans(mg1);
-	cv::Mat p1=polarTrans(f1);
+	int sizeB=sizeA/4;
+	cv::Rect rB={f1.cols/2-sizeB/2,f1.rows/2-sizeB/2,sizeB,sizeB};
+	cv::Mat p1=polarTrans(f1(rB));
+	int sizeC=sizeB/2;
+	cv::Rect rC={0,0,sizeB,sizeC};
 
 	cv::waitKey();
 
 	cv::Mat I2 = cv::imread(filename2, cv::IMREAD_GRAYSCALE);
-	cv::Mat mg2= I2(r);
+	cv::Mat mg2= I2(rA);
 	cv::Mat f2=fourierTrans(mg2);
-	cv::Mat p2=polarTrans(f2);
+	cv::Mat p2=polarTrans(f2(rB));
 	
-	cv::Point2d point=phaseCorr(p1, p2);
+	cv::Point2d point=phaseCorr(p1(rC), p2(rC));
 	std::cout<<point<<std::endl;
+	std::cout<<"Angle: "<<360*point.y/sizeB<<std::endl;
 	cv::waitKey();
 }
